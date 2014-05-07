4749426321576df9d7537ac70096319bd648a414
zh-wang <viennakanon@gmail.com>
2014-05-07 19:04:48 +0900

    add .gitignore

--- terminal_dictionary.pl
+++ terminal_dictionary.pl
@@ -16,8 +16,6 @@ use HTML::TagParser;
 #}
 #--------------
 
-# read from shell
-#--------------
 open F, "curl http://ejje.weblio.jp/small/content/".$ARGV[0]." -print|";
 #open F, "curl http://ejje.weblio.jp/content/".$ARGV[0]." -print|";
 
@@ -34,10 +32,6 @@ my %htmlMap;
 my @record = split /<!--開始 .*?-->/, $htmlContent;
 my @dicNames;
 
-#foreach my $a (@record) {
-    #print "$a\n====================\n";
-#}
-
 # parse returned html to a map
 my $i = 1;
 while ($htmlContent =~ /<!--開始 (.*?)-->/g) {
@@ -50,79 +44,38 @@ while ($htmlContent =~ /<!--開始 (.*?)-->/g) {
     #print $htmlMap{$1}."\n";
 }
 
-
 my $html = HTML::TagParser->new($htmlContent);
 
 my $number_to_show = 1;
 my $cont = $html->getElementById("content");
 my $contentSubTree = $cont->subTree();
 
-#if (&getDetailFromKennkyuusya() == undef) {
-   #&getDetailFromWikdictory(); 
-#}
-
-#my @division_list = $contentSubTree->getElementsByClassName("division2");
-#foreach my $elem ( @division_list ) {
-#    my $tagname = $elem->tagName;
-#    my $attr = $elem->attributes;
-#    my $text = $elem->innerText;
-##    print "<$tagname";
-##    foreach my $key ( sort keys %$attr ) {
-##        print " $key=\"$attr->{$key}\"";
-##    }
-##    if ( $text eq "" ) {
-##        print " />\n";
-##    } else {
-##        print ">$text</$tagname>\n";
-##    }
-#
-#    print "#-------------------------\n";
-#    my $divisionSubTree = $elem->subTree();
-#    my @subElemList = $divisionSubTree->getElementsByTagName("dt");
-#    if ($#subElemList + 1 == 0) {
-#        print "no result\n";
-#        last;
-#    }
-#    foreach my $subElem ( @subElemList ) {
-#        print $subElem->innerText."\n";
-#    }
-#
-#    $number_to_show--;
-#    if ($number_to_show == 0) {
-#        last;
-#    }
-#}
-
 my $script_path = abs_path($0);
 $script_path =~ /(.*)\//;
 my $script_directory = $1;
 
 my @division_list = $contentSubTree->getElementsByClassName("division2");
-open _OUTPUT, ">", $script_directory
-."/data/$ARGV[0].dic" or die "Cannot open output file to save searched word!";
+open _OUTPUT, ">", $script_directory."/data/$ARGV[0].dic" or die "Cannot open output file to save searched word!";
 
-#if (&wordExistd) {
-#if (&getDetailFromKennkyuusya() != undef) {
-    print _OUTPUT "========================"."\n";
+print _OUTPUT "========================"."\n";
+print "========================"."\n";
+$i = 0;
+foreach my $elem ( @division_list ) {
+    #print $dicNames[$i]."\n";
+    my $tagname = $elem->tagName;
+    my $attr = $elem->attributes;
+    my $text = $elem->innerText;
+    $i = $i + 1;
+    $text =~ s/[\n\r]+$ARGV[0]//;
+    $text =~ s/\./\.\n\r/g;
+    print $text."\n";
     print "========================"."\n";
-    $i = 0;
-    foreach my $elem ( @division_list ) {
-        #print $dicNames[$i]."\n";
-        my $tagname = $elem->tagName;
-        my $attr = $elem->attributes;
-        my $text = $elem->innerText;
-        $i = $i + 1;
-        $text =~ s/[\n\r]+$ARGV[0]//;
-        $text =~ s/\./\.\n\r/g;
-        print $text."\n";
-        print "========================"."\n";
-        print _OUTPUT $text."\n";
-        print _OUTPUT "========================"."\n";
-        if ($i > 0) {
-            last;
-        }
+    print _OUTPUT $text."\n";
+    print _OUTPUT "========================"."\n";
+    if ($i > 0) {
+        last;
     }
-#}
+}
 
 # 研究社
 sub getDetailFromKennkyuusya() {
