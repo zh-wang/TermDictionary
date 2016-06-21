ed611ead99873e7d558c8d24780ff95396df9287
zh-wang <viennakanon@gmail.com>
2016-06-21 15:42:53 +0900

    fix path problem

--- terminal_dictionary.pl
+++ terminal_dictionary.pl
@@ -6,9 +6,15 @@ scalar @ARGV >= 1 or exit say "Must provide a word";
 
 open F, "curl http://ejje.weblio.jp/content/".$ARGV[0]." -print|";
 
-my $script_path = abs_path($0); # Get the full absolute path of this pl file
-$script_path =~ /(.*)\//; # Get directory part
-my $script_directory = $1;
+sub script_dir_abs_path {
+    my $script_path = abs_path($0); # Get the full absolute path of this pl file
+    $script_path =~ /(.*)\//; # Get directory part
+    my $script_directory = $1;
+}
+
+my $script_directory = &script_dir_abs_path;
+say $script_directory;
+
 open OUT, ">", $script_directory."/data/$ARGV[0].dic"
     or die "Cannot open output file to save searched word!"; # Create a storage file
 
@@ -32,4 +38,4 @@ while ($htmlContent =~ /<!--開始 (?<DIC_NAME>.*?)-->(.*?)<!--終了 \k<DIC_NAM
     $i = $i + 1;
 }
 
-system("w3m -dump -T text/html data/.temp | less");
+system("w3m -dump -T text/html $script_directory/data/.temp | less");
