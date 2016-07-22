9447f54d25cc010e014d3efa0df9b4436e2c7022
zh-wang <viennakanon@gmail.com>
2016-07-22 11:29:16 +0900

    add error handling

--- terminal_dictionary.pl
+++ terminal_dictionary.pl
@@ -5,6 +5,7 @@ use Cwd 'abs_path';
 scalar @ARGV >= 1 or exit say "Must provide a word";
 
 open F, "curl http://ejje.weblio.jp/content/".$ARGV[0]." -print|";
+say "";
 
 sub script_dir_abs_path {
     my $script_path = abs_path($0); # Get the full absolute path of this pl file
@@ -37,5 +38,8 @@ while ($htmlContent =~ /<!--開始 (?<DIC_NAME>.*?)-->(.*?)<!--終了 \k<DIC_NAM
     say TEMP_OUT $2;
     $i = $i + 1;
 }
+
+# if $i still 0, then no contents can be showed. Maybe wrong spell in given word?
+$i > 0 or die "No contents. Maybe wrong spell in given word?";
 
 system("w3m -dump -T text/html $script_directory/data/.temp | less");
